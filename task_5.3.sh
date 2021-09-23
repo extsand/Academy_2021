98e25b369a5cdfac477c93014f6f8b60e9d21ac3
extsand <extsand@gmail.com>
2021-09-23 15:01:55 +0300

    update tasks

--- task_5.3.sh
+++ task_5.3.sh
@@ -85,8 +85,6 @@ ps | grep $some_app
 
 # 10. What information does top command display?
 # more https://www.maketecheasier.com/linux-top-explained/
-
-
 ## PID - process ID
 ## User - username
 ## PRI - priority
@@ -101,13 +99,46 @@ ps | grep $some_app
 ## COMMAND - command name / command line
 
 
-# 12. Display the processes of the specific user using the top command.
+# 11. Display the processes of the specific user using the top command.
+top | grep $username
+## top filterind
+top # then Shift+U then enter username 
+
 # 12. What interactive commands can be used to control the top command? Give a couple of
 # examples.
+## hot keys
+## Shift + N - sort by name
+## Shift + P - sort by CPU
+## Shift + M - sort by Memory
+## Shift + T - sort by Time
+## Shift + Z - change color of top output
+
+
 # 13. Sort the contents of the processes window using various parameters (for example, the
 # amount of processor time taken up, etc.)
+## hot keys
+## Shift + N - sort by name
+## Shift + P - sort by CPU
+## Shift + M - sort by Memory
+## Shift + T - sort by Time
+## Shift + Z - change color of top output
+
+
+
+########### ======= QUESTIONS =================
+### It is rigth? 
 # 14. Concept of priority, what commands are used to set priority?
+## value will be from -20 to 19 
+nice -n $value $app_name
+renice -n $value -p $PID
+## Higth priority - higth perfomance
+## Low priority - low perfomance
+
+
 # 15. Can I change the priority of a process using the top command? If so, how?
+
+
+
 # 16. Examine the kill command. How to send with the kill command
 # process control signal? Give an example of commonly used signals.
 # 17. Commands jobs, fg, bg, nohup. What are they for? Use the sleep, yes command to
