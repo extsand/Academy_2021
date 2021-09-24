53d59e4d642411b3c888a016d5f529cf6404919e
Alexander <extsand@gmail.com>
2021-09-25 01:18:30 +0300

    add file

--- 2.linux.networking/networking_task1.sh
+++ 2.linux.networking/networking_task1.sh
@@ -53,3 +53,5 @@ iface eth0 inet static
 	netmask 255.255.255.0
 	network 192.168.59.0
 	broadcast 192.168.59.255
+
+# see network map: https://github.com/extsand/Academy_2021/blob/linux_1/img/Networking_Task1_1.jpg
