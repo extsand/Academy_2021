4ee88796d29c9e12ff529d79422e36af2b6edb05
Alexander <extsand@gmail.com>
2021-09-25 00:51:46 +0300

    add file

--- 2.linux.networking/networking_task1.sh
+++ 2.linux.networking/networking_task1.sh
@@ -4,6 +4,8 @@ echo 'Networking tasks'
 
 #1. Create virtual machines connection according to figure :
 # more http://rus-linux.net/MyLDP/vm/VirtualBox-networking.html
+# tips and tricks
+# more https://ubuntugeeks.com/questions/23410/cannot-find-device-eth0
 #
 #|Itnernet|
 # ^|
@@ -14,3 +16,40 @@ echo 'Networking tasks'
 #|VM-2|
 #
 
+sudo lshw -C network #Check network devices
+sudo nano /etc/network/interfaces # create network
+#====================================
+eth0 # NAT
+#configure Host
+#WorkNetwork
+auto eth1
+iface eth1 inet static
+	address 192.168.58.1
+	netmask 255.255.255.0
+	network 192.168.58.0
+	broadcast 192.168.58.255
+#=====================================
+# configure VM1
+## WorkNetwork
+auto eth0
+iface eth0 inet static
+	address 192.168.58.2
+	netmask 255.255.255.0
+	network 192.168.58.0
+	broadcast 192.168.58.255
+## Adapter to VM2
+auto eth1
+iface eth1 inet static
+	address 192.168.59.1
+	netmask 255.255.255.0
+	network 192.168.59.0
+	broadcast 192.168.59.255
+
+#================================
+# configure VM2
+auto eth0
+iface eth0 inet static
+	address 192.168.59.2
+	netmask 255.255.255.0
+	network 192.168.59.0
+	broadcast 192.168.59.255
