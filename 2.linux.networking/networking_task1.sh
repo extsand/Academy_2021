9d208fe1397e8272739373a8f60da9b8f939069b
Alexander <extsand@gmail.com>
2021-09-24 20:16:23 +0300

    add file

--- 2.linux.networking/networking_task1.sh
+++ 2.linux.networking/networking_task1.sh
@@ -4,12 +4,13 @@ echo 'Networking tasks'
 
 #1. Create virtual machines connection according to figure :
 # more http://rus-linux.net/MyLDP/vm/VirtualBox-networking.html
-#				|Itnernet|
-#					|
-#				|host|
-#					|
-# 			|VM-1|
-#					|
-#   	  |VM-2|
+#
+#|Itnernet|
+#		|
+#|host|
+#		|
+#|VM-1|
+#		|
+#|VM-2|
 #
 
