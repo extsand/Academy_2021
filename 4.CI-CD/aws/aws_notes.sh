ef6561626959d48f8bb5146da00026776d899c3d
Alexander <extsand@gmail.com>
2021-10-06 14:32:08 +0300

    update ci-cd folder
    add aws, add notes

---
+++ 4.CI-CD/aws/aws_notes.sh
@@ -0,0 +1,34 @@
+#Python examples from AWS 
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/tutorials.html
+
+#Configure AWS Instances
+
+
+#Install httpd Server
+sudo yum update httpd
+sudo yum install httpd
+sudo service httpd restart
+
+
+l
+#Install AWS CLI 
+# See more
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.htm
+# windows
+# C:\Users\User-name\.aws
+| - config
+|	- credentials
+# SSH
+# C:\Users\Aleksandr\.ssh
+| - ssh_key
+| - ssh_key.pub
+| - known_hosts
+| - config
+#confis include:
+# Host gitlab.com
+# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
+
+# Host github.com
+# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
+
+
