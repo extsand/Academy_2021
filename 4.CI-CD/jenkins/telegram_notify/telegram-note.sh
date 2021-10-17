ef315459ed5b6f85e481f5529650f288ba92b3a9
Alexander <extsand@gmail.com>
2021-10-17 23:23:06 +0300

    telegram notify

---
+++ 4.CI-CD/jenkins/telegram_notify/telegram-note.sh
@@ -0,0 +1,13 @@
+# More about telegram notifications
+# https://42point.com/posts/telegram-notifications-jenkins
+
+#1. Create telegram bot
+#2. Get telegram bot api
+#3. Add bot to chanell
+#4. Set bot as admin
+#5. Get chat id from this chanell
+# 6. Create Credentials in Jenkins
+# 7. Use "withCredentials" in pipeline
+
+https://api.telegram.org/bot<TOKEN>/getUpdates
+
