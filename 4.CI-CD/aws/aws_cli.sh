a6a4b8ca24df1327662b7751344bf5ac84fd82cf
Alexander <extsand@gmail.com>
2021-11-05 23:57:39 +0200

    aws cli notes

---
+++ 4.CI-CD/aws/aws_cli.sh
@@ -0,0 +1,54 @@
+#Install AWS CLI
+#https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+# See more
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.htm
+# windows
+# C:\Users\User-name\.aws
+| - config
+|	- credentials
+# SSH
+# C:\Users\Aleksandr\.ssh
+| - ssh_key
+| - ssh_key.pub
+| - known_hosts
+| - config
+#confis include:
+# Host gitlab.com
+# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
+
+# Host github.com
+# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
+
+
+
+
+
+#AWS CLI
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+
+#for default profile
+aws configure 
+
+#for custom profile
+aws configure --profile custom-profile
+
+#see configuration
+aws configure list 
+
+#see configuration for custom profile
+aws configure list --profile custom-profile
+
+#to configure from file
+aws configure import --csv file://credentials.csv
+
+#info about profiles
+aws configure list-profiles
+
+
+ 
+
+
+
+AWS ecr access
+https://aws.amazon.com/ru/premiumsupport/knowledge-center/secondary-account-access-ecr/
--- 4.CI-CD/aws/aws_notes.sh
+++ 4.CI-CD/aws/aws_notes.sh
@@ -2,6 +2,9 @@
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/tutorials.html
 
 
+
+
+
 # Intstances log and user data
 # AWS -> Action -> Instance settings -> Edit User Dada 
 # 																	 ->
@@ -14,26 +17,8 @@ sudo yum update httpd
 sudo yum install httpd
 sudo service httpd restart
 
+#=============================================================================================
 
-l
-#Install AWS CLI 
-# See more
-# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.htm
-# windows
-# C:\Users\User-name\.aws
-| - config
-|	- credentials
-# SSH
-# C:\Users\Aleksandr\.ssh
-| - ssh_key
-| - ssh_key.pub
-| - known_hosts
-| - config
-#confis include:
-# Host gitlab.com
-# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
 
-# Host github.com
-# IdentityFile /c/Users/Aleksandr/.ssh/ssh_key
 
 
