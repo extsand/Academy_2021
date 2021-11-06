ca3ef6195d41d8974ffced0f2fb9ed86d2105cd3
Alexander <extsand@gmail.com>
2021-11-06 22:51:27 +0200

    add terragrunt info

---
+++ 4.CI-CD/terraform/terragrunt.sh
@@ -0,0 +1,19 @@
+# more about
+# https://terragrunt.gruntwork.io/docs/getting-started/install/#install-terragrunt
+
+# downloads file from:
+# https://github.com/gruntwork-io/terragrunt/releases
+#  terragrunt_linux_amd64
+
+
+# linux
+mv terragrunt_linux_amd64 terragrunt
+chmod u+x terragrunt
+mv terragrunt /usr/local/bin/terragrunt
+
+#windows
+#also download terragrunt_windows_amd64
+mkdir terraform
+add PATH to env.var
+
+
