c3be823e01526a0001a51294f79977dc8cf653f5
Alexander <extsand@gmail.com>
2021-11-08 11:12:35 +0200

    mod aws-cli

--- 4.CI-CD/aws/aws_cli.sh
+++ 4.CI-CD/aws/aws_cli.sh
@@ -1,5 +1,11 @@
 #Install AWS CLI
 #https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
 # See more
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.htm
 # windows
