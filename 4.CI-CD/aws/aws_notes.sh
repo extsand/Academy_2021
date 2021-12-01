462135d6d14dbd28e42bcac6a5e8ecfeb3f4c9fe
Alexander <extsand@gmail.com>
2021-12-01 02:06:09 +0200

    aws notes

--- 4.CI-CD/aws/aws_notes.sh
+++ 4.CI-CD/aws/aws_notes.sh
@@ -2,6 +2,16 @@
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/tutorials.html
 
 
+#Route 53
+# https://aws.amazon.com/ru/route53/faqs/
+
+#HealthCheck
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
+# https://aws.amazon.com/ru/premiumsupport/knowledge-center/troubleshoot-unhealthy-checks-ecs/
+
+#ECR Events
+# https://docs.aws.amazon.com/AmazonECR/latest/userguide/ecr-eventbridge.html
+
 
 
 
--- 4.CI-CD/tools/java_notes.sh
+++ 4.CI-CD/tools/java_notes.sh
@@ -17,11 +17,17 @@ jar cfv oracle.jar oracle/
 
 
 #notes for springboot 
+# https://howtodoinjava.com/spring-boot/spring-boot-ssl-https-example/
 #confirure
 logging.level.org.springframework.web: trace
 logging.level.org.apache: trace
 
 
-#tomcat
+#Tomcat
+#tomcat settings for aws
+# https://willwarren.com/2014/01/27/running-apache-tomcat-with-ssl-behind-amazon-elb/
+#simple page for tomcat
 # http://www.herongyang.com/JSP/Tomcat-My-First-HTML-Page.html
 docker run -it -p 80:8080 tomcat:latest
+#tricks with 443-80
+# https://stackoverflow.com/questions/53474735/tomcat-8080-behind-aws-application-load-balancer-443
